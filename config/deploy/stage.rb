ac14fb0e9aa538480f7345635060636d73fa833b
Erin Fahy <efahy@stanford.edu>
2016-07-22 15:45:35 -0700

    Clean up deploy and start up

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,4 +1,4 @@
-server 'sulbot-prod.stanford.edu', user: 'sulbot', roles: %w{app db web}
+server 'sulbot-stage.stanford.edu', user: 'sulbot', roles: %w{app db web}
 set :bundle_without, %w(test deployment development).join(' ')
 
 Capistrano::OneTimeKey.generate_one_time_key!
