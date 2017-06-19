264d1722bcd2f05704b81cb6acc76834d23c442c
Tony Zanella <tony.zanella@gmail.com>
2017-06-19 11:17:10 -0700

    cap patch party refactor

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,12 @@
 set :application, 'sulbot'
 set :repo_url, 'https://github.com/sul-dlss/sulbot.git'
-set :user, 'sulbot'
-set :home_directory, "/opt/app/#{fetch(:user)}"
+set :home_directory, '/opt/app/sulbot'
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "#{fetch(:home_directory)}/#{fetch(:application)}"
+set :deploy_to, '/opt/app/sulbot/sulbot'
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log pids node_modules}
